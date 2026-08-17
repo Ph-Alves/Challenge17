f842b2fa17332478060d1fbae5a8ee67f4c694c3
Ph-Alves <ph.costaalves@gmail.com>
2026-08-17 14:45:12 -0300

    Creating project

---
+++ Challenge17/Challenge17 Watch App/Challenge17App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge17App.swift
+//  Challenge17 Watch App
+//
+//  Created by Paulo Henrique Costa Alves on 17/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge17_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
