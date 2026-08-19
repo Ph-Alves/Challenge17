ad5cafba425752a09e657577d5b41b1c3a7ea1b5
Ph-Alves <ph.costaalves@gmail.com>
2026-08-19 15:23:36 -0300

    Created view and added assets

---
+++ Challenge17/Challenge17 Watch App/View/TutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialView.swift
+//  Challenge17 Watch App
+//
+//  Created by Paulo Henrique Costa Alves on 19/08/26.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialView()
+}
